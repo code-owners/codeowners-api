c29f6a7d5468cc11f1d2745f4f5f7dcbc787cfe5
elaygl <elaygl@gmail.com>
2018-03-19 18:08:18 +0200

    Match the paths with the same logic as github

--- src/utils/hasMatch.ts
+++ src/utils/hasMatch.ts
@@ -3,11 +3,14 @@ import {MappedData} from '../types';
 
 const startWithBackslash = (str: string) => (str.startsWith('/') ? str : `/${str}`);
 
-const hasMatch = (mappedFile: MappedData[], codeOwners: string[], path: string) =>
-    mappedFile.some(
-        x =>
-            x.owners.some(o => codeOwners.includes(o)) &&
-            anymatch(`${startWithBackslash(x.path)}*`, startWithBackslash(path))
-    );
+const hasMatch = (mappedFile: MappedData[], codeOwners: string[], path: string) => {
+    const match = mappedFile
+        .slice()
+        .reverse()
+        .find(x => anymatch(`${startWithBackslash(x.path)}*`, startWithBackslash(path)));
+    if (!match) return false;
+
+    return match.owners.some(o => codeOwners.includes(o));
+};
 
 export default hasMatch;
