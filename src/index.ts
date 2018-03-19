3ad9ca7429f305dae4fe2f8fac4cd77b4a76ea92
Ore Poran <ore@soluto.com>
2018-03-19 10:36:19 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -13,27 +13,18 @@ export interface CodeOwner {
     pathString: string;
 }
 
-export interface Path {
-    pathString: string;
-}
-
 export const initialize = (repoParams: GetCodeOwnersFileParams): void => {
     repoData = {...repoParams}
 };
 
 
-export const filterForCodeOwner = async (paths: string[], codeOwner: string): Promise<Path[]> => {
+export const filterForCodeOwner = async (paths: string[], codeOwner: string): Promise<string[]> => {
     const mappedFile = await mapCodeOwnersFile()
-    const matches = paths
-        .filter(requestedPath => hasSingleMatch(mappedFile, codeOwner, requestedPath))
-        .map(result => convertToPath(result))
-    return matches
+    return paths.filter(requestedPath => hasSingleMatch(mappedFile, codeOwner, requestedPath))
 };
 
 
 const hasSingleMatch = (mappedFile: MappedData[], codeOwner: string, path: string) => mappedFile.some(x => x.owners.includes(codeOwner) && anymatch(x.pathString, path))
-
-const convertToPath = (pathString: string):Path => ({pathString})
 
 const mapCodeOwnersFile = async (): Promise<MappedData[]> => {
     const data = await getCodeOwnersFile(repoData);
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import * as codeOwnersApi from '../src/index'
 
 (async () => {
-    const params = {repo: 'code-owners-api', owner: 'oreporan'}
+    const params = {repo: 'codeowners', owner: 'elaygl'}
     await codeOwnersApi.initializeRemote(params)
 })();
